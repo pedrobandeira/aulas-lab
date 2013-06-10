ae173373ec140e72d1c863b5ec7771f0f223b864
Pedro <Pedro@192.168.0.100>
2013-06-09 23:05:57 -0300

    Trabalho 11

---
+++ entregas/trabalho11/src/main/java/br/com/senacrs/alp/aulas/Factory.java
@@ -0,0 +1,22 @@
+package br.com.senacrs.alp.aulas;
+
+public class Factory {
+
+	private static final Factory instancia = new Factory();
+
+	private Factory() {
+	}
+
+	public ArquivoConfiguracao criarArquivoConfiguracao(String arquivoEntrada) {
+
+		ArquivoConfiguracao resultado = null;
+
+		resultado = new MeuArquivoConfiguracao(arquivoEntrada);
+
+		return resultado;
+	}
+
+	public static Factory getInstancia() {
+		return instancia;
+	}
+}
---
+++ entregas/trabalho12/src/main/java/br/com/senacrs/alp/aulas/MeuArquivo.java
@@ -0,0 +1,48 @@
+package br.com.senacrs.alp.aulas; import java.io.File;
+
+class MeuArquivo implements ArquivoConfiguracao, Configuracao {
+
+	private ArquivoConfiguracao config;
+	
+
+	MeuArquivo (ArquivoConfiguracao config){
+
+		if(config == null){
+			throw new IllegalArgumentException();
+		}
+
+		this.config = config;
+	}
+
+	@Override
+	public ArquivoConfiguracao getArquivoConfiguracao() {
+
+		ArquivoConfiguracao resultado = null;
+		return resultado;
+	}
+
+	@Override
+	public boolean valido() {
+
+		return false;
+	}
+
+	@Override
+	public String getRootDir() {
+
+		return null;
+	}
+
+	@Override
+	public String getErrorDir() {
+
+		return null;
+	}
+
+	@Override
+	public int getPort() {
+
+		return 0;
+	}
+
+}
