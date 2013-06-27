319f11d0680586def4132b4d965881a706946682
631102452 <631102452@Sala604-13.fspoaeduc.com.br>
2013-06-26 22:01:13 -0300

    Trabalho 17

---
+++ entregas/trabalho17/src/main/java/br/com/senacrs/alp/aulas/LeitorArquivos.java
@@ -0,0 +1,9 @@
+package br.com.senacrs.alp.aulas;
+
+public interface LeitorArquivos {
+
+	String[] lerArquivo(String arquivo);
+
+	String[] lerArquivoComSubstituicao(String arquivo, String busca,
+			String substituicao);
+}
